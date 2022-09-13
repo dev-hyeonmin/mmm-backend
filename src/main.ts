b48d10fb0bce2ba8f4ac95e93b157aac59c499ab
ROHHYEONMIN <roh._.hm@RohHyeonMinui-MacBookPro.local>
2022-09-13 17:57:58 +0900

    procfile

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log(process.env);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT || 4000);
 }
